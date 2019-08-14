604a8bae8607aa025365cb9dd3077a119f10318f
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-08-14 09:38:54 -0600

    Persist event data
    Call a function and pass it persisted event data

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,18 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react'
+
+export default class CoordinatesButton extends Component {
+
+  handleClick = event => {
+    let coordinates = [event.clientX, event.clientY]
+    this.props.onReceiveCoordinates(coordinates)
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}></button>
+      </div>
+    )
+  }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,20 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react'
+
+export default class DelayedButton extends Component {
+
+  handleClick = event => {
+    event.persist();
+    setTimeout(() => {this.props.onDelayedClick(event)}, this.props.delay)
+    // console.log(event)
+  }
+
+  render() {
+    // console.log(this.props.onDelayedClick)
+    return (
+      <div>
+        <button onClick={this.handleClick}></button>
+      </div>
+    )
+  }
+}
